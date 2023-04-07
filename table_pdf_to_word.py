12ebececf1bf5f764ec1e97678e9847b81462264
nezamtrm <128442592+nezamtrm@users.noreply.github.com>
2023-04-07 15:02:40 +0330

    add comments to table_pdf_to_word.py

--- table_pdf_to_word.py
+++ table_pdf_to_word.py
@@ -1,23 +1,44 @@
 
+# Import the 'Document' class from the 'docx' module
 from docx import Document
+
+# Import the 'pdfplumber' module
 import pdfplumber
+
+# Import the 'pandas' module and rename it to 'pd'
 import pandas as pd
 
+# Define a dictionary containing settings for table extraction from pdf using pdfplumber
 table_settings = {"vertical_strategy": 'text', 'horizontal_strategy': 'text'}
+
+# Open the pdf file using pdfplumber and assign the resulting object to variable 'pdf'
 pdf = pdfplumber.open('0_deep_survey.pdf')
+
+# Extract table data from page 17 of the pdf and assign it to variable 'table'
 table = pdf.pages[17].extract_table(table_settings)
+
+# Create a pandas DataFrame using the table data and assign it to variable 'df_main'
+# Ignore the first row as it is the table header, and set the column names using the first row of the table data
 df_main = pd.DataFrame(table[1::], columns= table[0])
 
+# Create a new Word document using the 'Document' class from the 'docx' module and assign it to variable 'doc'
 doc = Document()
-# add a table to the end and create a reference variable
-# extra row is so we can add the header row
+
+# Add a table to the end of the Word document using the 'add_table()' method of the 'Document' class,
+# and assign the resulting object to variable 't'
+# Add an extra row to the table so that we can add the header row separately
 t = doc.add_table(df_main.shape[0] + 1, df_main.shape[1])
-# add the header rows.
+
+# Add the header row to the table
 for j in range(df_main.shape[-1]):
+    # Set the text of the cell at row 0, column j to the jth column name of the DataFrame
     t.cell(0, j).text = df_main.columns[j]
-# add the rest of the data frame
+
+# Add the remaining data of the DataFrame to the table
 for i in range(df_main.shape[0]):
     for j in range(df_main.shape[-1]):
+        # Set the text of the cell at row i+1, column j to the value of the ith row and jth column of the DataFrame
         t.cell(i + 1, j).text = str(df_main.values[i, j])
-# save the doc
+
+# Save the Word document to a file named 'wordfile.docx'
 doc.save('wordfile.docx')
